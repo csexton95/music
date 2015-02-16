42cf5350427281f7c561f2364f11990cd8c815fc
csexton95 <csexton@sjccs142.saintjoe.edu>
2015-02-16 15:27:53 -0500

    added a new file

---
+++ music.java
@@ -0,0 +1,16 @@
+public class Music {
+	public String name;
+	public String artist;
+
+//---------------------------------------
+
+	Music(String thisName, String thisArtist) {
+	name = thisName;
+	artist = thisArtist;
+	}
+
+Music(String thisName) {
+
+	name = thisName;
+	}
+}
